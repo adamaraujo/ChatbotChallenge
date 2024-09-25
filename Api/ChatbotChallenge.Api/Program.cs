674b5178e40360f0f91ad14d05d0f3a9014dbe7c
Adam <adamcsaraujo@gmail.com>
2024-09-24 22:33:25 -0300

    chore: change swagger config

--- Api/ChatbotChallenge.Api/Program.cs
+++ Api/ChatbotChallenge.Api/Program.cs
@@ -29,11 +29,20 @@ var app = builder.Build();
 app.UseCors("AllowAll");
 
 // Configure the HTTP request pipeline.
+app.UseSwagger();
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
     app.UseSwaggerUI();
 }
+if (!app.Environment.IsDevelopment())
+{
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
+
 
 app.UseHttpsRedirection();
 
